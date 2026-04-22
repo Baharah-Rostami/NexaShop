8c253ca8f57f081f63490ada2cd42d46c7f59a55
Baharah Rostami <baharrustami888@gmail.com>
2026-04-22 13:33:20 +0430

    the carts now can be saved in localstorage

--- src/Store/store.js
+++ src/Store/store.js
@@ -1,8 +1,30 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./CartSlice";
 
+const loadState = () => {
+  try {
+    const serializedState = localStorage.getItem("cart");
+    return serializedState ? JSON.parse(serializedState) : undefined;
+  } catch (e) {
+    return undefined;
+  }
+};
+
+const saveState = (state) => {
+  try {
+    localStorage.setItem("cart", JSON.stringify(state));
+  } catch (e) {}
+};
+
 export const store = configureStore({
-    reducer:{
-        cart : cartReducer
-    }
+  reducer: {
+    cart: cartReducer,
+  },
+  preloadedState: {
+    cart: loadState(),
+  },
+});
+
+store.subscribe(() => {
+  saveState(store.getState().cart);
 });
